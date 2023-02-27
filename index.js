d7098e58d53a4b895756e715877e0c44698deeb3
Yoboii.Fuentes - Jose Fuentes <josexfuentes27@gmail.com>
2023-02-27 13:34:30 -0800

    added couple more commands from tutorial

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const fs = require("node:fs");
-const path = require("node:path");
+const fs = require("node:fs"); //reads from "commands" folder/dir I created and sees command files
+const path = require("node:path");	//will get the path for those files
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
+
+const commandsPath = path.join(__dirname, "commands");	//just gets the path: C:\Users\xfuen\Documents\Projects\WondoBot\commands
+const commandFiles = fs	//puts each of the command files into an array 
     .readdirSync(commandsPath)
     .filter((file) => file.endsWith(".js"));
 
